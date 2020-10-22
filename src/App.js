727ae08f4174aa915249582dd06821f87004f9e1
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-10-22 23:55:42 +0530

    Added GA compatibility

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import DesktopLayout from "./layouts/DesktopLayout"
 import MobileLayout from "./layouts/MobileLayout"
 
+import ReactGA from 'react-ga';
+
+const GATAG = 'G-4X58DZ9P6E';
+
+ReactGA.initialize(GATAG);
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 const isMobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
 
 const App = () => isMobile ? <MobileLayout /> : <DesktopLayout />;
